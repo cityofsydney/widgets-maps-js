6f8d85b5a1f163779d3d938c7c314739efd2f859
Paramjit <PKaur@cityofsydney.nsw.gove.au>
2020-09-24 14:09:09 +1000

    did testing for Lat long

--- webpack.config.js
+++ webpack.config.js
@@ -104,4 +104,17 @@ module.exports = [
       umdNamedDefine: true,
     },
   },
+  {
+    ...defaultConfig,
+    entry: "./src/bike-amenities/index.js",
+    output: {
+      path: distDir,
+      publicPath: "/",
+      filename: "bike-amenities.js",
+      library: "JsMapWidget",
+      libraryExport: "default",
+      libraryTarget: "umd",
+      umdNamedDefine: true,
+    },
+  },
 ];
